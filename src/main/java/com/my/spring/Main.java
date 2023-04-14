83d91ae853acbe2b1ce5413df548810f65444ed6
wangzehui <18500507445@163.com>
2023-04-14 10:18:42 +0800

    readme

--- src/main/java/com/my/spring/Main.java
+++ src/main/java/com/my/spring/Main.java
@@ -1,11 +1,12 @@
-package org.example;
+package com.my.spring;
 
 /**
  * @description:
  * @author: wzh
- * @date: 2023/4/14 09:37
+ * @date: 2023/4/14 10:18
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
