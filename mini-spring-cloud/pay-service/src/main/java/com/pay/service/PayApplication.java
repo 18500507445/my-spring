e71d1290ad08a285f18ebd383f7827ffbba1ed0e
wangzehui <18500507445@163.com>
2023-04-28 20:25:33 +0800

    暂存

---
+++ mini-spring-cloud/pay-service/src/main/java/com/pay/service/PayApplication.java
@@ -0,0 +1,17 @@
+package com.pay.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: wzh
+ * @date: 2023/4/28 20:24
+ */
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
